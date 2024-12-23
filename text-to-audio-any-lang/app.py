d04cb5d08a60a898fad3b1a53cd167d71cd17f7c
Ahmad <muhammadahmad@nxb.com.pk>
2024-12-23 18:29:36 +0500

    Enable the port

--- text-to-audio-any-lang/app.py
+++ text-to-audio-any-lang/app.py
@@ -44,5 +44,5 @@ def translate():
         print('Translation error:', e)
         return jsonify({"error": str(e)}), 500
 
-# if __name__ == '__main__':
-#     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
